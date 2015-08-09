4c45b0c1360c02d8f214ec4c32e4b93e00031abd
frank184 <francoisbelanger1993@hotmail.com>
2015-08-09 16:38:59 -0400

    Added Movie scaffold and Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_console
+  
+  def set_console
+    console
+  end
 end
